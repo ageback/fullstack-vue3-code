441ce2a482b6a4e3204f2fd34f6dec9354e0be73
Michael Lu <yeskky@gmail.com>
2023-05-18 15:50:10 +0800

    在父子组件之间传递状态（单向传递）

--- calendar_app/src/app/components/CalendarDay.vue
+++ calendar_app/src/app/components/CalendarDay.vue
@@ -1,7 +1,11 @@
 <script>
+import CalendarEvent from './CalendarEvent.vue';
 export default {
     name: 'CalendarDay',
-    props: ['day']
+    props: ['day'],
+    components: {
+        CalendarEvent
+    }
 }
 </script>
 
@@ -10,15 +14,7 @@ export default {
         <div class="day-banner has-text-centered">{{ day.abbvTitle }}</div>
         <div class="day-details">
             <div class="day-number">{{ day.id }}</div>
-            <div class="day-event" style="background-color: rgb(153,255,153);">
-                <div>
-                    <span class="has-text-centered details">Get Groceries</span>
-                    <div class="icons has-addons-centered">
-                        <i class="fa fa-pencil-square edit-icon"></i>
-                        <i class="fa fa-trash-o delete-icon"></i>
-                    </div>
-                </div>
-            </div>
+            <CalendarEvent v-for="(event, index) in day.events" :key="index" :event="event" :day="day" />
         </div>
     </div>
 </template>
@@ -80,4 +76,5 @@ export default {
             }
         }
     }
-}</style>
+}
+</style>
