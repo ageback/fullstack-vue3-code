7b8a54941cd57e435c71318146c92a8e9f637bfc
Michael Lu <yeskky@gmail.com>
2023-05-18 10:04:57 +0800

    使用计算属性排序

--- upvote/app/main.js
+++ upvote/app/main.js
@@ -4,6 +4,13 @@ const upvoteApp = {
             submissions: Seed.submissions
         }
     },
+    computed: {
+        sortedSubmissions() {
+            return this.submissions.sort((a, b) => {
+                return b.votes - a.votes
+            });
+        },
+    },
 };
 
 Vue.createApp(upvoteApp).mount("#app");
