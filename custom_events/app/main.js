40f4139e90dba91f3448710090694ef51153b402
Michael Lu <yeskky@gmail.com>
2023-05-19 14:28:45 +0800

    如何自定义事件，并在component中触发调用根实例的方法

--- custom_events/app/main.js
+++ custom_events/app/main.js
@@ -1,10 +1,52 @@
+/**
+事件触发过程：
+1. 在input-component实例中，松开Enter键触发keyup.enter事件监听器并调用 monitorEnterKey()
+2. monitorEnterKey() 方法发出 add-note 事件，并清除用户输入框
+3. <input-component></input-component> 元素上绑定的 add-note 事件监听器监听事件触发，并调用 root instance 的 addNote() 方法。
+4. addNote() 方法随即更新 root instance 的 data 属性，此属性会响应式渲染视图。
+*/
+
 const inputComponent = {
-  template: `<input class="input is-small" type="text" />`
-}
+  template: `<input 
+              :placeholder="placeholder" 
+              class="input is-small" 
+              type="text" 
+              @keyup.enter="monitorEnterKey"
+              v-model="input"  />`,
+  props: ["placeholder"],
+  data() {
+    return {
+      input: "",
+    };
+  },
+  emits: ["add-note"],
+  methods: {
+    monitorEnterKey() {
+      this.$emit("add-note", {
+        note: this.input,
+        timestamp: new Date().toLocaleString(),
+      });
+      this.input = "";
+    },
+  },
+};
 
 const app = {
+  data() {
+    return {
+      notes: [],
+      timestamps: [],
+      placeholder: 'Enter a note'
+    }
+  },
   components: {
     'input-component': inputComponent
+  },
+  methods: {
+    addNote(event) {
+      this.notes.push(event.note);
+      this.timestamps.push(event.timestamp);
+    }
   }
 };
 
